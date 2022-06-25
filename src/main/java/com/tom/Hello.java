dfbaa071c1078e910189f2d1691f79fe12354a8c
Nancy967 <beg7520@gmail.com>
2022-06-25 10:54:05 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -5,5 +5,16 @@ public class Hello {
 //        System.out.println("Hello World");
         Person p = new Person();
         p.hello();
+
+        int age = 21;
+        Integer age2 = 22;
+        char c = '我';
+        Character c2 = 'A';
+        byte b = 100;
+        float weight = 55.2f;
+        double height = 163.4;
+        boolean adult = true;
+        boolean entroll = false;
+        String name = "Nancy";
     }
 }
--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -4,6 +4,12 @@ fun main() {
 //    println("Hello Kotlin")
     val h = Human()
     h.hello()
+
+    var age = 22
+    age = 32
+    val weight = 73.2f
+    var name1 : String
+    name1 = "Mike"
 }
 
 class Human{
