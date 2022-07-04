4e7bbdbb4b35f8bd6b9d0a16259e6f25c5c2f93c
Nancy967 <beg7520@gmail.com>
2022-07-04 08:05:08 +0800

    reader

--- src/main/java/com/tom/io/Tester.java
+++ src/main/java/com/tom/io/Tester.java
@@ -1,20 +1,61 @@
 package com.tom.io;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 
 public class Tester {
     public static void main(String[] args) {
+        bufferedReader();
+//        fileReader();
+//        inputStream();
+    }
+
+    private static void bufferedReader() {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("data.txt"));
+            String line = br.readLine();
+            while (line != null){
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void fileReader() {
+        try {
+            FileReader fr = new FileReader("data.txt");
+            int n = fr.read();
+            while (n != -1){
+                System.out.print((char) n);
+                n = fr.read();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void inputStream() {
         File file = new File("data.txt");
         System.out.println(file.exists());
         System.out.println(file.getAbsolutePath());
         try {
             InputStream is = new  FileInputStream(file);
+            int n = is.read();
+            while (n != -1){
+                System.out.print((char) n);
+                n = is.read();
+            }
+            System.out.println();
         }catch (FileNotFoundException e){
             e.printStackTrace();
             System.out.println("Exception");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
         System.out.println("Hello!");
     }
